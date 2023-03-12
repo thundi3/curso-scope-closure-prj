fe40ff65d0eb29d6b2ab3ee775502c03ae77fdf7
Diego Quisupangui <animdiego@gmail.com>
2023-03-12 13:02:55 -0500

    debug JS

---
+++ src/debug/012_idex.js
@@ -0,0 +1,14 @@
+//Debug JS
+var a = 'Hello';
+
+function hello() {
+    let b = 'Hello World';
+    const c = 'Hello World.!!';
+    debugger;
+}
+
+hello();
+
+//copiar el codigo fuente
+//abirir un navegador y colocar "about:blank"
+//inspeccionar el codigo para usar CONSOLE y poder ejecutar el debugger
